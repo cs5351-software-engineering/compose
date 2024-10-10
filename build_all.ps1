8a63e5efc6e783170de01bab44487a986db8db02
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-10-10 22:39:59 +0800

    feat: update build_all.ps1 for frontend and olllama

--- build_all.ps1
+++ build_all.ps1
@@ -1,38 +1,54 @@
+# Clear the console
+Clear-Host
+
 # Store the current directory in a variable
 $currentDir = Get-Location
 
+# Function to build develop and release images
 function BuildDevelopReleaseImage {
     param (
         [string]$folder_name
     )
-    
-    Set-Location .\$folder_name\$folder_name\
+
+    # Set the working directory
+    Set-Location $currentDir\$folder_name\$folder_name\
 
     # Build develop image
-    Write-Host "\n[$folder_name] Building develop image..."
-    & .\build.develop.ps1
+    Write-Host "`n[$folder_name] Building develop image ..."
+    $output = & .\build.develop.ps1 *>&1
     
+    # Check if the develop image was built successfully
     if ($LASTEXITCODE -eq 0) {
-        Write-Host "Develop image built successfully." -ForegroundColor Green
+        Write-Host "[$folder_name] Develop image built successfully." -ForegroundColor Green
     } else {
-        Write-Host "Failed to build develop image." -ForegroundColor Red
+        Write-Host "[$folder_name] Failed to build develop image." -ForegroundColor Red
+        Write-Host $output # Show the output if it fails
         exit 1
     }
 
     # Build release image
-    Write-Host "\n[$folder_name] Building release image..."
-    & .\build.release.ps1
+    Write-Host "[$folder_name] Building release image ..."
+    $output = & .\build.release.ps1 *>&1
 
+    # Check if the release image was built successfully
     if ($LASTEXITCODE -eq 0) {
         Write-Host "[$folder_name] Release image built successfully." -ForegroundColor Green
     } else {
-        Write-Host "Failed to build release image." -ForegroundColor Red
+        Write-Host "[$folder_name] Failed to build release image." -ForegroundColor Red
+        Write-Host $output # Show the output if it fails
         exit 1
     }
 
 }
 
-BuildDevelopReleaseImage backend
+# try-catch-finally can handle ctrl+c and return to the original directory
+try {
+    # Build develop and release images for frontend, backend, and ollama
+    BuildDevelopReleaseImage frontend
+    BuildDevelopReleaseImage backend
+    BuildDevelopReleaseImage ollama
 
-# Return to the original directory
-Set-Location $currentDir
+} finally {
+    # Return to the original directory
+    Set-Location $currentDir
+}
